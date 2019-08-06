1e8b4ba274a3f1b107685e2f975099c081cacfaa
beyondszine <saurabhshandilya.1991@gmail.com>
2019-08-06 18:01:03 +0530

    generalised template of build status

--- slack/build-start-template.sh
+++
@@ -1,3 +0,0 @@
-
-curl --request POST --url "$SLACK_NOTIFICATION_URL" --header "$AUTH_HEADER_VEDALABS" --header 'content-type: application/json' \
---data '{"text": "'"Build start: $CI_PROJECT_NAME"'","attachments": [{"fallback": "'"Commit: $DOCKER_ENV_CI_COMMIT_SHORT_SHA"'", "author_name": "'"Owner: $GITLAB_USER_LOGIN"'", "title": "'"$CI_PROJECT_NAME: $DOCKER_ENV_CI_COMMIT_SHORT_SHA"'", "text": "'"$DOCKER_ENV_CI_COMMIT_TITLE"'" ,"actions": [{"name": "action", "type": "button","text": "View","style": "","value": "complete","url": "'"$DOCKER_ENV_CI_PROJECT_URL"'"}]}]}'
